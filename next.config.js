584b25d50bb41ab69655b5914f396a2ec9be6b6b
Torleif Halseth <halseth.torleif@gmail.com>
2023-06-09 08:07:35 +0200

    feat: define i18n config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n: {
+    locales: ["nb", "en"],
+    defaultLocale: "nb",
+    localeDetection: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
