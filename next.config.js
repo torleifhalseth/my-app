df1acf5380cbe80be72fa0710539fa4e97d6fef1
Torleif Halseth <halseth.torleif@gmail.com>
2023-06-09 08:14:48 +0200

    fix: remove i18n to test dynamic segment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  i18n: {
-    locales: ["nb", "en"],
-    defaultLocale: "nb",
-    localeDetection: false,
-  },
+  //   i18n: {
+  //     locales: ["nb", "en"],
+  //     defaultLocale: "nb",
+  //     localeDetection: false,
+  //   },
 };
 
 module.exports = nextConfig;
