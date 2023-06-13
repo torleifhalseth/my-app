58d3f5b50c6390376e491ea029efbac76c515836
Torleif Halseth <halseth.torleif@gmail.com>
2023-06-13 14:49:08 +0200

    wip: remove test of layout

--- src/app/guild/(layout)/(client)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-7xl mx-auto pt-10 px-6 md:px-10 lg:px-20">
-      {children}
-    </div>
-  );
-}
